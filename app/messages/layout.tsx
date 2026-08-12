30d830af92d6321fa730eab99e77212f3eb73f59
kalebw1999-star <kalebw1999@icloud.com>
2026-08-11 18:47:06 -0700

    Add voice invite controls to messages section

---
+++ app/messages/layout.tsx
@@ -0,0 +1,5 @@
+import VoiceInviteButton from "./voice-invite-button";
+
+export default function MessagesLayout({ children }: { children: React.ReactNode }) {
+  return <><VoiceInviteButton />{children}</>;
+}
